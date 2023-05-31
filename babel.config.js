4999aeb551af405a9bc56976379168e90a17f398
jhonataT <jhonata.tenorio2526@gmail.com>
2023-05-30 22:07:32 -0300

    Adding initial project structure

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+// to build: yarn babel src/index.js --out-file dist/bundle.js
+
+module.exports = {
+    presets: [
+        '@babel/preset-env',
+        ['@babel/preset-react', {
+            runtime: 'automatic'
+        }]
+    ]
+};
